88be5d26eb5706cd1eb4d68cc20926b3a62fff9f
Vitaly <vitalii6351109@gmail.com>
2022-12-05 02:18:31 +0600

    update player control

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import pygame as pg
 
 from player import Player
 from voxel_render import VoxelRender
+import settings
+
 
 class App:
     def __init__(self):
-        self.size = self.width, self.height = (800, 450)
+        pg.init()
+        pg.mouse.set_visible(False)
+        self.size = self.width, self.height = (settings.WIDTH, settings.HEIGHT)
         self.screen = pg.display.set_mode(self.size, pg.SCALED)
         self.clock = pg.time.Clock()
         self.player = Player()
         self.voxel_render = VoxelRender(self)
+        self.font = pg.font.SysFont('Arial', 15, bold=True)
 
     def update(self):
         self.player.update()
@@ -17,7 +22,28 @@ class App:
 
     def draw(self):
         self.voxel_render.draw()
+        self.fps(self.clock)
+        self.config()
         pg.display.flip()
+
+    def config(self):
+        config = pg.image.load('images//config.png')
+        config.set_colorkey((255, 255, 255))
+        scale = pg.transform.rotate(config, 180)
+        scale = pg.transform.scale(scale, (
+            config.get_width() // 100,
+            config.get_height() // 100
+        ))
+        scale_rect = scale.get_rect(center=(settings.WIDTH // 2 - scale.get_width() // 2,
+                                            settings.HEIGHT // 2 - scale.get_height() // 2))
+        self.screen.blit(scale, (scale_rect[0], scale_rect[1]))
+
+    def fps(self, clock):
+        display_fps = str(int(clock.get_fps()))
+        render_fps = self.font.render(display_fps, True, 'green')
+        render_height = self.font.render(str(self.player.height), True, 'blue')
+        self.screen.blit(render_fps, (settings.WIDTH - 20, 5))
+        self.screen.blit(render_height, (settings.WIDTH - 20, 20))
 
     def run(self):
         while True:
@@ -25,8 +51,8 @@ class App:
             self.draw()
 
             [exit() for event in pg.event.get() if event.type == pg.QUIT]
+
             self.clock.tick(60)
-            pg.display.set_caption(f'FPS: {self.clock.get_fps()}')
 
 
 if __name__ == '__main__':
--- player.py
+++ player.py
@@ -1,47 +1,79 @@
 import math
+from ctypes import POINTER, WINFUNCTYPE, windll
+from ctypes.wintypes import BOOL, HWND, RECT
 
 import pygame as pg
 import numpy as np
+from pyautogui import moveTo
+
+import voxel_render as vr
+import settings
 
 
 class Player:
     def __init__(self):
-        self.pos = np.array([0, 0], dtype=float)
+        self.pos = np.array([50, 50], dtype=float)
         self.angle = math.pi / 4
         self.height = 270
         self.pitch = 40
-        self.angle_velocity = 0.01
+        self.angle_velocity = 0.1
         self.velocity = 3
+        self.sensetivity = 0.002
 
     def update(self):
+        self.keys_control()
+        self.mouse_control()
+
+    def keys_control(self):
         sin_a = math.sin(self.angle)
         cos_a = math.cos(self.angle)
+        x = self.pos[0]
+        y = self.pos[1]
 
         pressed_key = pg.key.get_pressed()
-        if pressed_key[pg.K_UP]:
-            self.pitch += self.velocity
-        if pressed_key[pg.K_DOWN]:
-            self.pitch -= self.velocity
 
-        if pressed_key[pg.K_LEFT]:
-            self.angle -= self.angle_velocity
-        if pressed_key[pg.K_RIGHT]:
-            self.angle += self.angle_velocity
+        if pressed_key[pg.K_ESCAPE]:
+            exit()
 
-        if pressed_key[pg.K_q]:
-            self.height += self.velocity
-        if pressed_key[pg.K_e]:
-            self.height -= self.velocity
+        if 0 < x and len(vr.height_map) > x and 0 < y and len(vr.height_map) > y:
 
-        if pressed_key[pg.K_w]:
-            self.pos[0] += self.velocity * cos_a
-            self.pos[1] += self.velocity * sin_a
-        if pressed_key[pg.K_s]:
-            self.pos[0] -= self.velocity * cos_a
-            self.pos[1] -= self.velocity * sin_a
-        if pressed_key[pg.K_a]:
-            self.pos[0] += self.velocity * sin_a
-            self.pos[1] -= self.velocity * cos_a
-        if pressed_key[pg.K_d]:
-            self.pos[0] -= self.velocity * sin_a
-            self.pos[1] += self.velocity * cos_a
+            self.height = vr.height_map[int(x), int(y)][0] + 20
+
+            if pressed_key[pg.K_w]:
+                self.pos[0] += self.velocity * cos_a
+                self.pos[1] += self.velocity * sin_a
+            if pressed_key[pg.K_s]:
+                self.pos[0] -= self.velocity * cos_a
+                self.pos[1] -= self.velocity * sin_a
+            if pressed_key[pg.K_a]:
+                self.pos[0] += self.velocity * sin_a
+                self.pos[1] -= self.velocity * cos_a
+            if pressed_key[pg.K_d]:
+                self.pos[0] -= self.velocity * sin_a
+                self.pos[1] += self.velocity * cos_a
+
+    def mouse_control(self):
+        if pg.mouse.get_focused():
+            difference_x = pg.mouse.get_pos()[0] - settings.HALF_WIDTH
+            difference_y = pg.mouse.get_pos()[1] - settings.HALF_HEIGHT
+            pg.mouse.set_pos((settings.HALF_WIDTH, settings.HALF_HEIGHT))
+            self.angle += difference_x * self.sensetivity
+            self.pitch -= difference_y
+        else:
+            window_x, window_y = self.get_window_coords()
+            try:
+                moveTo(window_x + settings.HALF_WIDTH + pg.SCALED, window_y + settings.HALF_HEIGHT + pg.SCALED // 2)
+            except Exception:
+                pass
+            finally:
+                pass
+
+    def get_window_coords(self):
+        hwnd = pg.display.get_wm_info()['window']
+        prototype = WINFUNCTYPE(BOOL, HWND, POINTER(RECT))
+        paramflags = (1, 'hwnd'), (2, 'lprect')
+
+        GetWindowRect = prototype(('GetWindowRect', windll.user32), paramflags)
+
+        rect = GetWindowRect(hwnd)
+        return rect.top, rect.left

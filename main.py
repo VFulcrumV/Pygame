88e59214c0ec0567ff73305a5ffc8e232fd336e7
Vitaly <vitalii6351109@gmail.com>
2022-12-04 17:18:58 +0600

    Add base code

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print('hello')
+import pygame as pg
+
+from player import Player
+from voxel_render import VoxelRender
+
+class App:
+    def __init__(self):
+        self.size = self.width, self.height = (800, 450)
+        self.screen = pg.display.set_mode(self.size, pg.SCALED)
+        self.clock = pg.time.Clock()
+        self.player = Player()
+        self.voxel_render = VoxelRender(self)
+
+    def update(self):
+        self.player.update()
+        self.voxel_render.update()
+
+    def draw(self):
+        self.voxel_render.draw()
+        pg.display.flip()
+
+    def run(self):
+        while True:
+            self.update()
+            self.draw()
+
+            [exit() for event in pg.event.get() if event.type == pg.QUIT]
+            self.clock.tick(60)
+            pg.display.set_caption(f'FPS: {self.clock.get_fps()}')
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
---
+++ player.py
@@ -0,0 +1,47 @@
+import math
+
+import pygame as pg
+import numpy as np
+
+
+class Player:
+    def __init__(self):
+        self.pos = np.array([0, 0], dtype=float)
+        self.angle = math.pi / 4
+        self.height = 270
+        self.pitch = 40
+        self.angle_velocity = 0.01
+        self.velocity = 10
+
+    def update(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+
+        pressed_key = pg.key.get_pressed()
+        if pressed_key[pg.K_UP]:
+            self.pitch += self.velocity
+        if pressed_key[pg.K_DOWN]:
+            self.pitch -= self.velocity
+
+        if pressed_key[pg.K_LEFT]:
+            self.angle -= self.angle_velocity
+        if pressed_key[pg.K_RIGHT]:
+            self.angle += self.angle_velocity
+
+        if pressed_key[pg.K_q]:
+            self.height += self.velocity
+        if pressed_key[pg.K_e]:
+            self.height -= self.velocity
+
+        if pressed_key[pg.K_w]:
+            self.pos[0] += self.velocity * cos_a
+            self.pos[1] += self.velocity * sin_a
+        if pressed_key[pg.K_s]:
+            self.pos[0] -= self.velocity * cos_a
+            self.pos[1] -= self.velocity * sin_a
+        if pressed_key[pg.K_a]:
+            self.pos[0] += self.velocity * sin_a
+            self.pos[1] -= self.velocity * cos_a
+        if pressed_key[pg.K_d]:
+            self.pos[0] -= self.velocity * sin_a
+            self.pos[1] += self.velocity * cos_a
